8bed5f4c0aa1368843f65fc4296887e4872ef9c2
Zelnesh <zelnesh7777@gmail.com>
2026-04-25 23:52:23 +0000

    Finished all user input ports args cases and input validation

--- internal/portscanner/scanner.go
+++ internal/portscanner/scanner.go
@@ -35,38 +35,100 @@ func scan(ip, port string, results chan string, wg *sync.WaitGroup) {
 }
 
 
+func portArgInputCharValidation(s string) bool {
+	for _, ch := range s{
+		if (ch >= '0' && ch <= '9') || ch == '-' || ch == ',' {
+			continue
+		}
+		return false
+	}
+	return true
+}
+
+
 func ScanPort (ip, port string) <-chan string {
 
 	var wg sync.WaitGroup
 	results := make(chan string)
-	if strings.Contains(port, ",") {
-		ports := strings.Split(port, ",")
-
+	if !portArgInputCharValidation(port){
 		go func(){
+			results <- "Wrong port argument. Please check documentation using --help"
+			close(results)
+		}()
+		return results
+	} else if strings.Contains(port, "-") && strings.Contains(port, ","){
+		go func(){
+			results <- "Wrong port argument. Please check documentation using --help"
+			close(results)
+		}()
+		return results
+
+	}else if strings.Contains(port, ",") {
+		ports := strings.Split(port, ",")
+		for _, p := range ports {
+			if p == "" {
+				go func(){
+					results <- "Worng port argument. Please check ocumentation using --help"
+					close(results)
+				}()
+				return results
+			}
+		}
+
+		go func() {
 			for _, port := range ports {
-			    wg.Add(1)
-			    go scan(ip,strings.TrimSpace(port), results, &wg)
+				wg.Add(1)
+				go scan(ip, strings.TrimSpace(port), results, &wg)
 			}
 			wg.Wait()
 			close(results)
 		}()
-	}
 
-	if strings.Contains(port, "-"){
+	} else if strings.Contains(port, "-") {
 		ports := strings.Split(port, "-")
+		if len(ports) != 2 {
+			go func(){
+				results <- "Worng port range argument. Please check documentation using --help"
+				close(results)
+			}()
+			return results
+		}
 		startPort, _ := strconv.Atoi(ports[0])
 		endPort, _ := strconv.Atoi(ports[1])
 
-		go func(){
-			for port := startPort; port <= endPort; port++{
+		go func() {
+			for port := startPort; port <= endPort; port++ {
 				wg.Add(1)
 				go scan(ip, strconv.Itoa(port), results, &wg)
 			}
 			wg.Wait()
 			close(results)
+		}()
+	} else {
+		portCheck, err := strconv.Atoi(port)
+		if err != nil {
+			go func(){
+				results <- "Port should be an integer number..."
+				close(results)
+			}()
+			return results
+		}
+		if portCheck < 1 || portCheck > 65535 {
+			go func(){
+				results <- "Port out of range, range could be 1-65535..."
+				close(results)
+			}()
+			return results
+		}
+		go func(){
+			wg.Add(1)
+			go scan(ip, strconv.Itoa(portCheck), results, &wg)
+			wg.Wait()
+			close(results)
 
 		}()
 	}
+
 
 	return results
 }
